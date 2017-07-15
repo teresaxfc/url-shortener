f8fec48ad6e6de53ca7ac91bae23fe847c133d73
teresa <teresaxfc@gmail.com>
2017-07-15 22:57:55 +1000

    Use babel to transcompile all files under src/client folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
 var webpack = require('webpack');
 var path = require('path');
 
-var APP_DIR = path.resolve(__dirname, 'src/client/app');
+var APP_DIR = path.resolve(__dirname, 'src/client');
 var BUILD_DIR = path.resolve(__dirname, 'public/js');
 
 var config = {
-  entry: APP_DIR + '/index.jsx',
+  entry: APP_DIR + '/app/index.jsx',
   output: {
     path: BUILD_DIR,
     filename: 'bundle.js'
